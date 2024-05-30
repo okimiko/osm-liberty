767168ea2c77294c59959fb3daea6694d728939c
pathmapper <pathmapper@posteo.de>
2024-05-30 14:48:51 +0200

    Use spreet instead of spritezero
    https://github.com/flother/spreet maintained, faster, smaller sprites...

--- build_sprites.sh
+++ build_sprites.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir temp
-cp -a ./svgs/svgs_not_in_iconset/. ./temp/ 
-cp -a ./svgs/svgs_iconset/. ./temp/ 
-./node_modules/.bin/spritezero ./sprites/osm-liberty ./temp/
-./node_modules/.bin/spritezero --retina ./sprites/osm-liberty@2x ./temp/
+mkdir -p ./temp
+wget -P ./temp https://github.com/flother/spreet/releases/download/v0.11.0/spreet-x86_64-unknown-linux-musl.tar.gz
+tar -xzvf ./temp/spreet-x86_64-unknown-linux-musl.tar.gz -C ./temp
+./temp/spreet --unique --minify-index-file --recursive ./svgs ./sprites/osm-liberty
+./temp/spreet --unique --minify-index-file --recursive --retina ./svgs ./sprites/osm-liberty@2x
 rm -r temp
