748d1c1c336b21b9529270f7db05c1d41d35b830
pathmapper <pathmapper@posteo.de>
2024-05-30 15:33:17 +0200

    Fix sprite json

--- build_sprites.sh
+++ build_sprites.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir -p ./temp
+mkdir -p ./temp/svgs
+cp -a ./svgs/svgs_not_in_iconset/. ./temp/svgs/
+cp -a ./svgs/svgs_iconset/. ./temp/svgs/
 wget -P ./temp https://github.com/flother/spreet/releases/download/v0.11.0/spreet-x86_64-unknown-linux-musl.tar.gz
 tar -xzvf ./temp/spreet-x86_64-unknown-linux-musl.tar.gz -C ./temp
-./temp/spreet --unique --minify-index-file --recursive ./svgs ./sprites/osm-liberty
-./temp/spreet --unique --minify-index-file --recursive --retina ./svgs ./sprites/osm-liberty@2x
+./temp/spreet --unique --minify-index-file --recursive ./temp/svgs/ ./sprites/osm-liberty
+./temp/spreet --unique --minify-index-file --recursive --retina ./temp/svgs/ ./sprites/osm-liberty@2x
 rm -r temp
